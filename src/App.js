6df82877aa2d58209b5fa86288fac9ed40496ad6
ZhcLo <zhangchixx8@gmail.com>
2018-07-21 22:22:16 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        app
-      </div>
+		<ul className="navigation">
+			<li><a>首页</a></li>
+			<li><a>精华</a></li>
+			<li><a>关于</a></li>
+			<li><a>登录</a></li>
+		</ul>
     );
   }
 }
